332564110556184ac1fd3f9758812670bb0dfac6
Chris Lin <chris522229197@gmail.com>
2016-11-09 17:08:24 -0800

    Add Item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109025336) do
+ActiveRecord::Schema.define(version: 20161110010457) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.float    "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
